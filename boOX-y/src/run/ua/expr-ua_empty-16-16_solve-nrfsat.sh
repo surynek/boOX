24d72cc522f092618de590cdeddc3c2a25c856e2
surynek <pavel.surynek@seznam.cz>
2026-06-08 22:40:23 +0200

    Minor fix in unassigned agents experiment.

--- boOX-y/src/run/ua/expr-ua_empty-16-16_solve-nrfsat.sh
+++ boOX-y/src/run/ua/expr-ua_empty-16-16_solve-nrfsat.sh
@@ -11,5 +11,5 @@ UNASSIGNED_LIST=`cat $UNASSIGNED_FILE`
 for UNASSIGNED in $UNASSIGNED_LIST;
 do
     echo 'Solving '$PREFIX' scenario '$SCENARIO' MAPF instance with '$UNASSIGNED' unassigned agents of '$AGENTS' agents...'
-    ../../main/rota_solver_boOX '--timeout='$TIMEOUT  '--input-file='$SCENARIO_PREFIX'-'$SCENARIO'_a'$AGENTS'_u'$UNASSIGNED'.npf' '--algorithm=nrfsat' '--output-file=solution_'$SCENARIO_PREFIX'-'$SCENARIO'_a'$AGENTS'_u'$UNASSIGNED'.txt' > 'out_'$PREFIX'-'$SCENARIO'_a'$AGENTS'_u'$UNASSIGNED'.txt'
+    ../../main/rota_solver_boOX '--timeout='$TIMEOUT  '--input-file='$SCENARIO_PREFIX'-'$SCENARIO'_a'$AGENTS'_u'$UNASSIGNED'.npf' '--algorithm=nrfsat' '--output-file=solution_'$SCENARIO_PREFIX'-'$SCENARIO'_a'$AGENTS'_u'$UNASSIGNED'.txt' > 'out_'$PREFIX'-'$SCENARIO'_a'$AGENTS'_u'$UNASSIGNED'-nrfsat.txt'
 done
